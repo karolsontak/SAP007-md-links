0cf3d13cafc589d7f72ab7c09333e0a429a0aedd
Karol Sontak <karolsontak@gmail.com>
2022-06-17 01:13:05 -0300

    Função mdlinks

--- index.js
+++ index.js
@@ -16,3 +16,33 @@ function findFiles(dir, files) {
 
 module.exports = findFiles;
 
+const mdLinks = (directory) => {
+  return new Promise((resolve, reject) => {
+    const regex = /(\[.[^[\]]*\])(\([^#][^)]+)/gm;
+    const findedFiles = findFiles(directory, []);
+    if (findedFiles.length === 0) {
+      reject("Nenhum arquivo foi encontrado")
+    }
+    const results = findedFiles.map(fileMD => {
+      const file = fs.readFileSync(fileMD, 'utf8')
+      const match = file.match(regex);
+      if (match) {
+        return match.map((str) => {
+          const arrLink = str.replace('[', '').split('](');
+          const strObject = {
+            text: arrLink[0],
+            link: arrLink[1],
+            file: fileMD,
+          };
+          return strObject;
+        })
+      } else {
+        return null;
+      }
+    },
+    )
+    resolve(results)
+  })
+}
+
+module.exports = mdLinks;
