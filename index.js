9799be910e707f4b318d93e520806ebca34a23f9
Karol Sontak <karolsontak@gmail.com>
2022-06-17 01:11:23 -0300

    Função de leitura de arquivo

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+const path = require('path');
+
+function findFiles(dir, files) {
+    const directory = fs.readdirSync(dir);
+    return directory.reduce((getFiles, file) => {
+        const getDir = path.join(dir, file);
+        if (fs.statSync(getDir).isDirectory()) {
+            findFiles(getDir, getFiles);
+        } else if ((path.extname(file) === ".md")) {
+            getFiles.push(getDir);
+        }
+        return getFiles;
+    }, files || [])
+}
+
+module.exports = findFiles;
+
