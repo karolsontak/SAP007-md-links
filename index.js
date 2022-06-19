fe1d1bab2ead9a47252979691eb17d3f1f631393
Karol Sontak <karolsontak@gmail.com>
2022-06-19 03:00:20 -0300

    Tentativa de função integrada para ler diretório e arquivo

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 
+const dirPath = fs.readdirSync(__dirname);
+dirPath.forEach(file => {
+  if (path.extname(file) === ".md")
+console.log(file)
+})
+
+// function dirPath(file) {
+//   const absolutePath = path.join(__dirname, '.', file);
+//   const encoding = "utf8";
+//   try {
+//     const files = fs.readdir(absolutePath, { encoding });
+//     const result = files.map((file) => {
+//       const local = `${absolutePath}/${file}`;
+//       const text = fs.readFileSync(local, encoding);
+//     return findLinks(text);
+//   })
+//   return result;
+//   } catch (erro) {
+//     handleError(erro);
+//   } finally {
+//     console.log('Completed Operation');
+//   }
+
 function filePath(file) {
   const encoding = "utf8";
   try {
